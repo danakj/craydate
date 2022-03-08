14811e5985d517c032573e636965be6344e5210e
Dana Jansens <danakj@orodu.net>
2022-03-07 21:56:18 -0500

    Format lib.rs with rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
-use playdate_sys::PlaydateAPI;
 use playdate_sys::PDSystemEvent;
+use playdate_sys::PlaydateAPI;
 use std::ffi::CString;
 
-
 #[no_mangle]
-pub extern "C" fn eventHandler(api: *mut PlaydateAPI, event: PDSystemEvent, _arg: u32) -> ::std::os::raw::c_int {
-
+pub extern "C" fn eventHandler(
+    api: *mut PlaydateAPI,
+    event: PDSystemEvent,
+    _arg: u32,
+) -> ::std::os::raw::c_int {
     let cstr = CString::new("hello world maybe").unwrap();
     unsafe {
         let system = *(*api).system;
@@ -13,7 +15,7 @@ pub extern "C" fn eventHandler(api: *mut PlaydateAPI, event: PDSystemEvent, _arg
     }
 
     if event == PDSystemEvent::kEventInit {
-        // set event handler here.        
+        // set event handler here.
     }
 
     // send me $5 and I'll comment yikes on your undocumented return value
