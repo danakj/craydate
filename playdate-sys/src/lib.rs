b31407309389684020680657cc5c0050c135d4b3
Dana Jansens <danakj@orodu.net>
2022-03-08 22:11:59 -0500

    arm chars are unsigned, use u8.

--- playdate-sys/src/lib.rs
+++ playdate-sys/src/lib.rs
@@ -1,3 +1,4 @@
+#![deny(clippy::all)]
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
@@ -5,7 +6,7 @@
 
 pub mod ctypes {
     pub type c_int = i32;
-    pub type c_char = i8;
+    pub type c_char = u8;
     pub type c_uint = u32;
     pub type c_ulonglong = u64;
     pub type c_void = core::ffi::c_void;
