60c4c6f06eb8a2a0003c6ec8376645e0a8791ec9
Dana Jansens <danakj@orodu.net>
2022-03-11 21:03:52 -0500

    Ensure libwindup is linked into the DLL

--- simulator-win/src/lib.rs
+++ simulator-win/src/lib.rs
@@ -1,5 +1,7 @@
 #![no_std]
 
+extern crate windup;
+
 #[panic_handler]
 fn panic_handler(info: &core::panic::PanicInfo) -> ! {
     // The #[panic_handler] must be in the top level crate, but we foward to the
