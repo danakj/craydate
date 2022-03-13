f85397331f4aaa5d1b0212752d64c060085e036a
Dana Jansens <danakj@orodu.net>
2022-03-13 14:29:00 -0400

    Make debug::log not unsafe, it no longer takes a Executor pointer argument

--- windup/playdate/src/debug.rs
+++ windup/playdate/src/debug.rs
@@ -1,6 +1,6 @@
 use crate::ctypes::*;
 use crate::CStr;
 
-pub unsafe fn _log(system: &'static CSystem, bytes: &[u8]) {
+pub fn _log(system: &'static CSystem, bytes: &[u8]) {
   system.logToConsole.unwrap()(CStr::from_bytes_with_nul(bytes).unwrap().as_ptr())
 }
