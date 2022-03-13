a8769c005740f93ffb7017a8ca1131c731c59076
Dana Jansens <danakj@orodu.net>
2022-03-13 14:29:37 -0400

    Fix the now-safe debug::_log function

--- windup/playdate/src/debug.rs
+++ windup/playdate/src/debug.rs
@@ -2,5 +2,5 @@ use crate::ctypes::*;
 use crate::CStr;
 
 pub fn _log(system: &'static CSystem, bytes: &[u8]) {
-  system.logToConsole.unwrap()(CStr::from_bytes_with_nul(bytes).unwrap().as_ptr())
+  unsafe { system.logToConsole.unwrap()(CStr::from_bytes_with_nul(bytes).unwrap().as_ptr()) }
 }
