29129af9abf4d4083c22ad18793baaf60c3adc65
Dana Jansens <danakj@orodu.net>
2022-03-19 22:15:03 -0400

    Comments on LCDBitmapData

--- windup/playdate/src/graphics.rs
+++ windup/playdate/src/graphics.rs
@@ -327,6 +327,7 @@ impl AsMut<LCDBitmapRef> for LCDBitmapRef {
   }
 }
 
+/// Metadata for an `LCDBitmap`.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct LCDBitmapData {
   width: i32,
@@ -335,15 +336,22 @@ pub struct LCDBitmapData {
   hasmask: i32,
 }
 impl LCDBitmapData {
+  /// The number of pixels (or, columns) per row of the bitmap.
+  /// 
+  /// Each pixel is a single bit, and there may be more bytes (as determined by `row_bytes()`) in a
+  /// row than required to hold all the pixels.
   pub fn width(&self) -> i32 {
     self.width
   }
+  /// The number of rows in the bitmap.
   pub fn height(&self) -> i32 {
     self.height
   }
+  /// The number of bytes per row of the bitmap.
   pub fn row_bytes(&self) -> i32 {
     self.rowbytes
   }
+  /// Whether the bitmap has a mask attached, via `set_mask_bitmap()`.
   pub fn has_mask(&self) -> bool {
     self.hasmask != 0
   }
