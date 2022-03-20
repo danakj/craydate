ac9e8487b68ac8a3b670bf6d4e15e3575a321750
Adrienne Walker <enne@quisquo.us>
2022-03-20 16:00:45 -0700

    Add world object, restructure hackiness

--- windup/src/gameloop.rs
+++ windup/src/gameloop.rs
@@ -26,68 +26,88 @@ pub fn fmin(v1: f32, v2: f32) -> f32 {
   cmp::min::<FloatOrd<f32>>(FloatOrd::<f32>(v1), FloatOrd::<f32>(v2)).0
 }
 
+pub struct AccumInputs {
+  crank_accum: f32,
+}
+impl AccumInputs {
+  pub fn accumulate(&mut self, inputs: &Inputs) {
+    if let &Crank::Undocked { angle: _, change } = inputs.crank() {
+      self.crank_accum += change;
+      self.crank_accum = fmax(fmin(self.crank_accum, CRANK_MAX), -CRANK_MAX);
+    }
+  }
+}
+
+pub struct World {
+  player: GameObj,
+  // TODO: add other stuff in the world
+}
+impl World {
+  fn player_update(&mut self, inputs: &Inputs, accum: &mut AccumInputs) {
+    let player = &mut self.player;
+    if inputs.buttons().left_state() == ButtonState::Pushed {
+      player.pos = player.pos.translate(euclid::default::Vector2D::new(-1, 0));
+    }
+    if inputs.buttons().right_state() == ButtonState::Pushed {
+      player.pos = player.pos.translate(euclid::default::Vector2D::new(1, 0));
+    }
+    for (button, event) in inputs.buttons().all_events() {
+      if event != ButtonEvent::Push {
+        continue;
+      }
+      if button == Button::A {
+        player.pos.origin.x = INITIAL_X;
+      }
+      // Only can jump when on the ground.
+      if button == Button::Up && player.pos.min_y() == FLOOR_Y {
+        let crank_sign: f32 = if accum.crank_accum < 0.0 { -1.0 } else { 1.0 };
+
+        let crank: f32 = fmin(accum.crank_accum.abs() / CRANK_FACTOR, CRANK_MAX).into();
+
+        accum.crank_accum = 0.0;
+        player.vel = euclid::default::Vector2D::new(crank * crank_sign, -crank)
+      }
+    }
+
+    player.vel.y += GRAVITY;
+
+    let mut remain = player.pos_remainder;
+    remain += player.vel;
+    let rounded = remain.round();
+    player.pos = player.pos.translate(rounded.to_i32());
+    player.pos_remainder = remain - rounded;
+
+    // """collision"""
+    if player.pos.origin.y >= FLOOR_Y {
+      player.pos.origin.y = FLOOR_Y;
+      player.vel = euclid::default::Vector2D::new(0.0, 0.0);
+    }
+    if player.pos.origin.x < MIN_X {
+      player.pos.origin.x = MIN_X;
+    }
+    if player.pos.origin.x > MAX_X {
+      player.pos.origin.x = MAX_X;
+    }
+  }
+
+  pub fn update(&mut self, inputs: &Inputs, accum: &mut AccumInputs) {
+    self.player_update(inputs, accum);
+  }
+
+  pub fn draw(&self, g: &mut Graphics) {
+    // TODO: draw other stuff in world
+    self.player.draw(g);
+  }
+}
+
 pub struct GameObj {
   bitmap: LCDBitmap,
   pos: euclid::default::Rect<i32>,
   // stuff that doesn't fit into integer pos
   pos_remainder: euclid::default::Vector2D<f32>,
   vel: euclid::default::Vector2D<f32>,
-  // FIXME: this shouldn't live here, refactor into a better input handler
-  crank_accum: f32,
 }
 impl GameObj {
-  // FIXME: this is really the player's update
-  pub fn update(&mut self, inputs: &Inputs) {
-    if let &Crank::Undocked { angle: _, change } = inputs.crank() {
-      self.crank_accum += change;
-      self.crank_accum = fmax(fmin(self.crank_accum, CRANK_MAX), -CRANK_MAX);
-    }
-
-    for (button, event) in inputs.buttons().all_events() {
-      if event != ButtonEvent::Push {
-        continue;
-      }
-      if button == Button::Left {
-        self.pos = self.pos.translate(euclid::default::Vector2D::new(-1, 0));
-      }
-      if button == Button::Right {
-        self.pos = self.pos.translate(euclid::default::Vector2D::new(1, 0));
-      }
-      if button == Button::A {
-        self.pos.origin.x = INITIAL_X;
-      }
-      // Only can jump when on the ground.
-      if button == Button::Up && self.pos.min_y() == FLOOR_Y {
-        let crank_sign: f32 = if self.crank_accum < 0.0 { -1.0 } else { 1.0 };
-
-        let crank: f32 = fmin(self.crank_accum.abs() / CRANK_FACTOR, CRANK_MAX).into();
-
-        self.crank_accum = 0.0;
-        self.vel = euclid::default::Vector2D::new(crank * crank_sign, -crank)
-      }
-    }
-
-    self.vel.y += GRAVITY;
-
-    let mut remain = self.pos_remainder;
-    remain += self.vel;
-    let rounded = remain.round();
-    self.pos = self.pos.translate(rounded.to_i32());
-    self.pos_remainder = remain - rounded;
-
-    // """collision"""
-    if self.pos.origin.y >= FLOOR_Y {
-      self.pos.origin.y = FLOOR_Y;
-      self.vel = euclid::default::Vector2D::new(0.0, 0.0);
-    }
-    if self.pos.origin.x < MIN_X {
-      self.pos.origin.x = MIN_X;
-    }
-    if self.pos.origin.x > MAX_X {
-      self.pos.origin.x = MAX_X;
-    }
-  }
-
   fn draw(&self, g: &mut Graphics) {
     g.draw_bitmap(
       &self.bitmap,
@@ -102,15 +122,16 @@ pub async fn run(mut api: playdate::Api) -> ! {
   let system = &mut api.system;
   let graphics = &mut api.graphics;
 
-  let player = GameObj {
-    bitmap: graphics.load_bitmap("images/bot").unwrap(),
-    pos: euclid::rect(INITIAL_X, FLOOR_Y, 32, 32),
-    pos_remainder: euclid::vec2(0.0, 0.0),
-    vel: euclid::vec2(0.0, 0.0),
-    crank_accum: 0.0,
+  let mut world = World {
+    player: GameObj {
+      bitmap: graphics.load_bitmap("images/bot").unwrap(),
+      pos: euclid::rect(INITIAL_X, FLOOR_Y, 32, 32),
+      pos_remainder: euclid::vec2(0.0, 0.0),
+      vel: euclid::vec2(0.0, 0.0),
+    },
   };
 
-  let mut objs = [player];
+  let mut accum = AccumInputs { crank_accum: 0.0 };
 
   let fw = system.frame_watcher();
   loop {
@@ -119,15 +140,9 @@ pub async fn run(mut api: playdate::Api) -> ! {
     // TODO: probably need a more efficient drawing mechanism than full redraw
     graphics.clear(LCDSolidColor::kColorWhite);
 
-    for obj in &mut objs {
-      obj.update(&inputs);
-    }
-
-    for obj in &objs {
-      obj.draw(graphics);
-    }
-
-    let hacky_intrusive_crank_value = objs[0].crank_accum;
+    accum.accumulate(&inputs);
+    world.update(&inputs, &mut accum);
+    world.draw(graphics);
 
     graphics.draw_text(
       "turn crank, hit up to jump",
@@ -137,7 +152,7 @@ pub async fn run(mut api: playdate::Api) -> ! {
     );
     graphics.draw_text("hit A to reset", PDStringEncoding::kASCIIEncoding, 5, 15);
 
-    let crank_str = format!("{:.1}", hacky_intrusive_crank_value);
+    let crank_str = format!("{:.1}", accum.crank_accum);
     graphics.draw_text(&crank_str, PDStringEncoding::kASCIIEncoding, 5, 30);
     graphics.draw_fps(400 - 15, 0);
   }
