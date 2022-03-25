079598a088683fb50bbf4388439856f3980cb5be
Dana Jansens <danakj@orodu.net>
2022-03-24 22:07:49 -0400

    Add the constants for the display size and rowbytes

--- windup/playdate/src/ctypes_enums.rs
+++ windup/playdate/src/ctypes_enums.rs
@@ -6,3 +6,7 @@ pub use playdate_sys::LCDSolidColor;
 pub use playdate_sys::PDLanguage;
 pub use playdate_sys::PDPeripherals;
 pub use playdate_sys::PDStringEncoding;
+
+pub const LCD_COLUMNS: u32 = playdate_sys::LCD_COLUMNS;
+pub const LCD_ROWS: u32 = playdate_sys::LCD_ROWS;
+pub const LCD_ROWBYTES: u32 = playdate_sys::LCD_ROWSIZE;
